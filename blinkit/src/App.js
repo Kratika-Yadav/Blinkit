ed386f486cf8e71114fabb4bb956941b9bb000d3
Kratika-Yadav <kycsbe15@gmail.com>
2024-02-15 10:23:31 +0530

    first commit

---
+++ blinkit/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import {BrowserRouter , Routes , Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import SearchGrocery from './components/SearchGrocery';
+import GroceryDescription from './components/GroceryDescription';
+import AddGrocery from './components/AddGrocery';
+import Signup from './components/Signup';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/usercart' element={<Cart/>}/>
+        <Route path='/search' element={<SearchGrocery/>}/>
+        <Route path='/product-desc/:prid' element={<GroceryDescription/>}/>
+        
+        
+        <Route path='/add' element={<AddGrocery/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+export default App;
